00d5d9c7473e71468b74fabbdd6cdd032f80c40f
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 09:00:40 +0700

    Update 2.5.py

--- Bài 3/2.5.py
+++ Bài 3/2.5.py
@@ -1,5 +1,5 @@
 
-print("########################")
+print('ho ten: Tran Minh Hieu; mssv245752021610155')
 #viet chuong trinh
 a = "hello Guy!"
 def say():
@@ -8,3 +8,4 @@ a = " Vinh University "
 print (a)
 say()
 print(a)
+
