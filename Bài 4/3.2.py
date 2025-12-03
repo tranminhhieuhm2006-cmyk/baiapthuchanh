4a60835cba4da2502ac351dd764fe106147dd0d2
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 09:05:29 +0700

    Update 3.2.py

--- Bài 4/3.2.py
+++ Bài 4/3.2.py
@@ -1,10 +1,11 @@
 
-print("##################################")
+print('ho ten: Tran Minh Hieu; mssv245752021610155')
 
 s = input('Nhap chuoi:')
 for ch in s:
     if ch.strip(): #Bo qua dau space va tab
         print(ch.upper(), end='')
+
 
 
 
