cd825acd44ee3ad62cc8336125457aad195952fa
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 09:02:07 +0700

    Update 3.10.py

--- Bài 4/3.10.py
+++ Bài 4/3.10.py
@@ -1,7 +1,8 @@
 
-print("##################################")
+print('ho ten: Tran Minh Hieu; mssv245752021610155')
 ds = input('nhap chuoi: ').split()
 x = ds[0:-1]
 for c in x:
     print(c)
     
+
