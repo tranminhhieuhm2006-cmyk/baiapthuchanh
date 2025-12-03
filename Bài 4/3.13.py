59cec3c2987ef1e61d55c3d085df72e9c348b1ac
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 09:03:02 +0700

    Update 3.13.py

--- Bài 4/3.13.py
+++ Bài 4/3.13.py
@@ -1,7 +1,8 @@
 
-print("##################################")
+print('ho ten: Tran Minh Hieu; mssv245752021610155')
 #nhap vao list
 ds = input('nhap chuoi: ').split()
 
 #tim phan tu trong list
 print("vi tri cua chuoi abc la", ds.index('abc'))
+
