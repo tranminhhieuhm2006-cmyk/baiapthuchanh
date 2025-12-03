aa1454eb85b96d57819fe17cd2f69a7c13804c21
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 08:46:27 +0700

    Refactor print statements and maintain sum function

--- Bài 3/2.1.py
+++ Bài 3/2.1.py
@@ -1,5 +1,5 @@
 
-print("########################")
+print("ho ten:tran minh hieu:msv 245752021610155")
 
 def sum(a, b):
  print("sum = " + str(a+b))
@@ -7,3 +7,4 @@ def sum(a, b):
 sum(5, 8)
 # tinh tong 2 so 0,4
 sum(0, 4)
+
