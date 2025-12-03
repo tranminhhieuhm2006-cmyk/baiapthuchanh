80c3fe3bd2df8c15ce04535d066293fe4a9c995b
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 09:06:59 +0700

    Update 3.25.py

--- Bài 4/3.25.py
+++ Bài 4/3.25.py
@@ -1,5 +1,6 @@
 
-print("################################")
+print('ho ten: Tran Minh Hieu; mssv245752021610155')
 numbers = input("Nhập danh sách số, cách nhau bởi dấu phẩy: ").split(',')
 odd_numbers = [num for num in numbers if int(num) % 2 != 0]
 print(','.join(odd_numbers))
+
