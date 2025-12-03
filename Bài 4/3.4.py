f2861c2d1e6041d045227e0576b6bf81045cd3d3
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-03 09:08:52 +0700

    Update 3.4.py

--- Bài 4/3.4.py
+++ Bài 4/3.4.py
@@ -1,5 +1,5 @@
 
-print("##################################")
+print('ho ten: Tran Minh Hieu; mssv245752021610155')
 
 ds = input('danh sach:').split( )
 
@@ -9,3 +9,4 @@ print(ds)
 #in day vua nhap, moi phan tu tren mot dong
 for so in ds:
     print(so)
+
