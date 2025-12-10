c9e9e7502627e97011bb0f716eef1c1811877edc
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-10 08:32:43 +0700

    Update 3.10.py

--- Bài 4/3.10.py
+++ Bài 4/3.10.py
@@ -1,8 +1,9 @@
-
-print('ho ten: Tran Minh Hieu; mssv245752021610155')
+print("Tran Minh Hieu")
+print("_msv:245752021610155")
 ds = input('nhap chuoi: ').split()
 x = ds[0:-1]
 for c in x:
     print(c)
     
+
 
