035ca998c35bfe77032874dbbc42f294b445a47a
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-10 08:18:57 +0700

    Update 2.11.py

--- Bài 2 (2)/Bài 2/2.11.py
+++ Bài 2 (2)/Bài 2/2.11.py
@@ -1,4 +1,5 @@
-
+print("Tran Minh Hieu")
+print("_msv:245752021610155")
 #chuong trinh noi danh sach vao tu dien
 l= [1,'python',4,7]
 k= ['cse',2,'guntur',8]
@@ -8,3 +9,4 @@ m.append(k);
 print(m)
 d= {1:1,2:k,'combine_list':m}
 print(d)
+
