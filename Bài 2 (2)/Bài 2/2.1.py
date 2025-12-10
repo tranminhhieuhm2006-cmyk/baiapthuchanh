0e0155a3ee68d90fb3ee937683ba575a987ac649
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-10 08:07:40 +0700

    Update 2.1.py

--- Bài 2 (2)/Bài 2/2.1.py
+++ Bài 2 (2)/Bài 2/2.1.py
@@ -1,4 +1,5 @@
-
+print("Tran Minh Hieu")
+print("_msv: 245752021610155
 n1 = int(input("enter n1 value"))
 n2 = int(input("enter n2 value"))
 if n1>n2:
@@ -6,3 +7,4 @@ if n1>n2:
 else:
     print("n2 is big")
     
+
