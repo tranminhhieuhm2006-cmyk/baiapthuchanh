ff06885a836c191576f357c6f7debc5ae394e11c
tranminhhieuhm2006-cmyk <tranminhhieuhm2006@gmail.com>
2025-12-10 08:16:31 +0700

    Update 2.10.py

--- Bài 2 (2)/Bài 2/2.10.py
+++ Bài 2 (2)/Bài 2/2.10.py
@@ -1,9 +1,10 @@
 print("Tran Minh Hieu")
-print("_msv:245752021610155)
+print("_msv:245752021610155")
 #chuong trinh tach nhap xau ki tu
 a = "hi i am python programmer"
 b = a.split()
 print(b)
 c ="".join(b)
 print(c)
+
 
